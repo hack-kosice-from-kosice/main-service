3e7cca47ff785530b49e590c88b83f09b6dee26e
Peter Vasiľ <peter.vasil@zonky.cz>
2020-09-05 12:15:01 +0200

    Initial commit

--- src/main/java/io/hackkosicefromkosice/mainservice/controller/VersionController.java
+++ src/main/java/io/hackkosicefromkosice/mainservice/controller/VersionController.java
@@ -12,7 +12,7 @@ public class VersionController {
     @GetMapping("/version-info")
     public Map<String, Object> index() {
         return new HashMap<String, Object>() {{
-           put("version", 1);
+           put("version", 2);
         }};
     }
 
